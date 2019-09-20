e5e795dfb7cbcfc0bce6fd652c2c2f439854d198
Yijiang Huang <yijiangh@mit.edu>
2019-09-20 23:27:12 +0200

    pip install done.

---
+++ src/ur_online_control/__version__.py
@@ -0,0 +1,8 @@
+__title__ = 'ur_online_control'
+__description__ = 'IP control package for UR robot'
+__url__ = 'https://github.com/createchaos/ur_online_control'
+__version__ = '0.0.1'
+__author__ = 'Romana Rust, Stefana Parascho, et al.'
+__author_email__ = 'parascho@princeton.edu'
+__license__ = 'MIT license'
+__copyright__ = 'Copyright 2019 CREATE lab @ Princeton University'
